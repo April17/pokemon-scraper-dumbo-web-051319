18570b2d0380557de2d1cc6de0f3ab97946d4db8
April17 <fanmeng0417@gmail.com>
2019-05-31 11:51:05 -0400

    first commit

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,12 @@
 class Pokemon
+  attr_accessor :name, :hp, :type
+  attr_reader :id
+
+  def initialize(name, hp, type, id = nil)
+    @id = id
+    @name = name
+    @hp = hp
+    @type = type
+  end
+
 end
